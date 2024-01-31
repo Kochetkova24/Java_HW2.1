7ba2e29a4b1bd3e94c2d2c67659d4c8c5732374a
Kochetkova Tanya <kta240190@gmail.com>
2024-01-31 22:31:57 +0500

    added progect

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int cost = 10000;
+        int amount = 20;
+        int miles = cost/amount;
+        System.out.println("За покупку Вашего билета начислено бонусов:"+miles);
+       }
+    }
